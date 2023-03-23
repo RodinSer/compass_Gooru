71459846e0ea803988afacb71d4533d5d7962663
Sr <rodin_sr>
2023-03-23 21:59:46 +0300

    merge

--- app/src/main/java/com/example/gooru/di/ViewModelModule.kt
+++ app/src/main/java/com/example/gooru/di/ViewModelModule.kt
@@ -5,11 +5,11 @@ import com.example.gooru.feature.presentation.authorization.registration.Registr
 import com.example.gooru.feature.presentation.chat.chat.ChatViewModel
 import com.example.gooru.feature.presentation.chat.tikets.TicketsViewModel
 import com.example.gooru.feature.presentation.home.HomeViewModel
-import com.example.gooru.feature.presentation.parser.addparsource.AddParSourceViewModel
-import com.example.gooru.feature.presentation.parser.parser.parserp.ParserViewModel
-import com.example.gooru.feature.presentation.parser.parser.favorite.FavoriteParserViewModel
-import com.example.gooru.feature.presentation.parser.parsource.MyParSourceViewModel
 import com.example.gooru.feature.presentation.profile.ProfileViewModel
+import com.example.gooru.feature.presentation.tabparsource.parser.addparsource.AddParSourceViewModel
+import com.example.gooru.feature.presentation.tabparsource.parser.parser.favorite.FavoriteParserViewModel
+import com.example.gooru.feature.presentation.tabparsource.parser.parser.parserp.ParserViewModel
+import com.example.gooru.feature.presentation.tabparsource.parsource.MyParSourceViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.qualifier.named
